e5657fba63db151b0b6e4ae21f3c231c93ea0638
ddragnell <andressalbecerra@gmail.com>
2023-09-18 17:46:38 -0500

    Feat: Spiderman.ico

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <center>
+            <h1>Bienvenido a Personajes de Marvel</h1>
+            <p>En esta app podras visualizar todos los personajes de Marvel y mas informacion sobre ellos</p>
+            <NuxtLink to="/personajes">
+                Click para visualizar 
+            </NuxtLink>
+        </center>
+
+    </div>
+    <div>
+        <p></p>
+    </div>
+</template>
+<script>
+definePageMeta({
+    layout: "default",
+});
+
+</script>
