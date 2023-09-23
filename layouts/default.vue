2afa5b5b6b10711d159d0d6ca38a5829d6f3ec9c
ddragnell <andressalbecerra@gmail.com>
2023-09-23 16:25:00 -0500

    Feat: Layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-layout class="rounded rounded-md">
+
+		<v-app-bar class="navbar" style="background-color: black;">
+			<a href="https://developer.marvel.com" class="logo"  >
+					<v-img src="/marvel.png" alt="Página Marvel" width="200" style="margin-left: 300%;"/>
+			</a>
+		</v-app-bar>
+
+	</v-layout>
+
+	<main class="content">
+		<slot />
+
+	</main>
+
+</template>
+
+
+<script setup>
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,7 @@
 <template>
-    <div>
-        <center>
-            <h1>Bienvenido a Personajes de Marvel</h1>
-            <p>En esta app podras visualizar todos los personajes de Marvel y mas informacion sobre ellos</p>
-            <NuxtLink to="/personajes">
-                Click para visualizar 
-            </NuxtLink>
-        </center>
-
-    </div>
-    <div>
-        <p></p>
+    <div class="ma-5">
+        <TasksCreateDialog />
+        <TasksTable />
     </div>
 </template>
-<script>
-definePageMeta({
-    layout: "default",
-});
-
-</script>
+  
