397ccec4977bf8b36e6898ee2870c9519ea96289
ddragnell <andressalbecerra@gmail.com>
2023-09-23 17:50:33 -0500

    Feat: Template redirecciona

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
 	<v-layout class="rounded rounded-md">
 
-		<v-app-bar class="navbar" style="background-color: black;">
-			<a href="https://developer.marvel.com" class="logo"  >
+		<v-app-bar style="background-color:black;">
+			<a href="https://developer.marvel.com" target="_blank">
 					<v-img src="/marvel.png" alt="Página Marvel" width="200" style="margin-left: 300%;"/>
 			</a>
 		</v-app-bar>
