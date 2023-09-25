d4cdc83af0cdc825860aa2af6f03b6879da31989
ddragnell <andressalbecerra@gmail.com>
2023-09-24 23:51:40 -0500

    Change: Layout class

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,15 @@
 
 		<v-app-bar style="background-color:black;">
 			<a href="https://developer.marvel.com" target="_blank">
-					<v-img src="/marvel.png" alt="Página Marvel" width="200" style="margin-left: 300%;"/>
+				<v-img src="/marvel.png" alt="Página Marvel" width="200" style="margin-left: 300%;" />
 			</a>
 		</v-app-bar>
 
+		<main class="align-center justify-center">
+			<slot />
+
+		</main>
 	</v-layout>
-
-	<main>
-		<slot />
-
-	</main>
-
 </template>
 
 
